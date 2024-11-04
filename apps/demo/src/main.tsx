9fc22b526c944e72005ab12d72db06c49f7dd2d5
seb <sebastien@mynotary.fr>
2024-11-04 10:51:44 +0100

    refactor: add file replacement repro

--- apps/demo/src/main.tsx
+++ apps/demo/src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-
-import App from './app/app';
-import './styles.none';
+import {environment} from "./env";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <div>Hello {environment.currentEnvFile} !</div>
   </StrictMode>
 );
